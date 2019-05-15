7e8814f087f6a5d9a1bd2376c99acb658ec0418d
Syed Sayem <syed@sayem.org>
2019-05-15 03:06:16 -0400

    Update dotfiles

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -6,19 +6,9 @@ killall -q polybar
 # puase while killall completes
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-if type "xrandr" > /dev/null; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload top -c ~/.config/polybar/config &
-	done
-else
-	polybar --reload top -c ~/.config/polybar/config &
-fi
-
 # Launch bar(s)
-polybar i3wmthemer_bar -q &
+MONITOR=HDMI-1 polybar example -c /home/sayem/.config/polybar/config &
 # polybar top -q &
 # polybar bottom -q &
 
 echo "polybars launched"
-
-albert &at
